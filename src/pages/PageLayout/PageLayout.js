c4b56dd0a3567e6c70ff1abc72c565ca41a29f28
Khadija_AlSelini <khadija.al-selini@ibm.com>
2022-08-05 21:36:35 +0100

    added y scroll for screen

--- src/pages/PageLayout/PageLayout.js
+++ src/pages/PageLayout/PageLayout.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { use100vh } from 'react-div-100vh';
 
 import './PageLayout.scss';
 
 const PageLayout = ({ children }) => {
+  const height = use100vh();
+  const halfHeight = height ? height : '50vh';
+
   return (
     <div className="body-wrap boxed-container">
       <main>
-        <section className="hero">
+        <section className="hero" style={{ height: halfHeight }}>
           <div className="container">{children}</div>
         </section>
       </main>
